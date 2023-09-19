7c2127ea8c55779b0732ea9e495d4dd8bee235d5
nikhiljose18 <nikhiljose18@gmail.com>
2023-09-19 23:04:27 +0530

    Initial Conmmit

---
+++ preview-gui/src/App.tsx
@@ -0,0 +1,23 @@
+import { Fragment, useEffect, useState } from "react";
+import "./App.css";
+
+// import os from 'os';
+
+// export const desktopPath = path.join(os.homedir(), 'Desktop');
+
+function App() {
+  // const [billData, setBillData] = useState("");
+
+  // const readJSON = ()=>{
+
+  // }
+
+  useEffect(() => {}, []);
+  return (
+    <Fragment>
+      <div>hnfedfiouhgiloudfhnobiudfh</div>
+    </Fragment>
+  );
+}
+
+export default App;
