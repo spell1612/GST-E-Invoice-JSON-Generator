4be247b127dcf020537ca73d641aa5efb19938b7
Sayooj Sojen <spell1612@gmail.com>
2023-10-24 01:31:30 +0530

    Updated rungui command to use only vite

--- scripts/run-gui.js
+++ scripts/run-gui.js
@@ -1,7 +1,7 @@
 import { exec } from 'child_process';
 
 export default () => {
-    exec('cd preview-gui && ..\\bin\\nodejs\\npx.cmd vite --open && exit', (error, stdout, stderr) => {
+    exec('cd preview-gui && ..\\bin\\nodejs\\vite.cmd --open && exit', (error, stdout, stderr) => {
         console.log({ error, stdout, stderr })
     })
 }
